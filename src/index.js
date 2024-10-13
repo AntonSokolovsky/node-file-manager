907fbe8895307c1112dc9bff52aa71bde9a0fb42
antonsokolovsky <anton.a.sokolovsky@gmail.com>
2024-10-13 16:51:38 +0300

    feat: add up command and goodbye message

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-const { stdin, stdout, cwd } = process;
+import { homedir } from "os";
+import readline from "readline";
+import path from "path";
+
+const { stdin, stdout, argv, cwd, chdir } = process;
 
 const username =
-  process.argv.find((arg) => arg.startsWith("--username=")).split("=")[1] ||
-  "Guest";
+  argv.find((arg) => arg.startsWith("--username="))?.split("=")[1] ||
+  "Anonymous";
 console.log(`Welcome to the File Manager, ${username}!`);
 
+chdir(homedir());
+
 console.log(`You are currently in ${cwd()}`);
+
+const rl = readline.createInterface({
+  input: stdin,
+  output: stdout,
+});
+
+rl.on("line", async (input) => {
+  if (input === "up") {
+    const currentDirectory = cwd();
+    const parentDirectory = path.resolve(currentDirectory, "..");
+    chdir(parentDirectory);
+    console.log(`You are currently in ${cwd()}`);
+  }
+});
+
+rl.on("close", () => {
+  console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+  process.exit();
+});
